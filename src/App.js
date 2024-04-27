b48e072f2a84e3c708f3e26e09f4cd634a5a25f2
athifal <101647187+athifal@users.noreply.github.com>
2024-04-27 20:36:41 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,49 @@
+
+import './App.css';
+import Aboutlite from './components/Aboutlite';
+import Banner from './components/Banner';
+
+import Chooseus from './components/Chooseus';
+import Footer from './components/Footer';
+import Gallarylite from './components/Gallarylite';
+import Header from './components/Header';
+import Logo from './components/Logo';
+import Pop from './components/Pop';
+import Serviceslite from './components/Serviceslite';
+import Testimonials from './components/Testimonials';
+
+
+
+function App() {
+  return (
+    <div>
+      <Pop></Pop>
+     <Header></Header>
+     <Banner></Banner>
+     <Aboutlite></Aboutlite>
+     <Chooseus></Chooseus>
+     <Serviceslite></Serviceslite>
+     <Gallarylite></Gallarylite>
+     
+
+     
+    
+     
+
+    
+     
+
+     
+     
+    <Testimonials></Testimonials>
+    
+    
+    <Logo></Logo>
+    <Footer></Footer>
+
+     
+    </div>
+  );
+}
+
+export default App;
